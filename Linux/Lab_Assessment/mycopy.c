09879c4f3612487791d408f80972d5bb0cbe402b
vepurivinaykumar <41463079+vepurivinaykumar@users.noreply.github.com>
2018-11-27 14:53:17 +0530

    Add files via upload

---
+++ Linux/Lab_Assessment/mycopy.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <pthread.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h> 
+#include <string.h> 
+#include <err.h> 
+
+#define MAX_MSG 32
+#define 
+static pthread_t th[4];
+
+
+int main (void) {
+}
+
+
