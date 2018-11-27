01ddf178e5941d30767a8ca34f6fc86694ea27ad
vepurivinaykumar <41463079+vepurivinaykumar@users.noreply.github.com>
2018-11-27 13:07:17 +0530

    Add files via upload

---
+++ Assignment2/source/one_mod/check.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <sys/ioctl.h>
+#include <errno.h> 
+
+int main (int arg_cnt, char **cmdArg)
+{
+    int fd;
+    int num = 12;
+    fd = open (cmdArg[1], O_RDWR);
+    while (1){
+        printf (" number:"); 
+        scanf ("%d", &num);
+        num = ioctl (fd, num, &num);
+        perror ("ioctl");
+        printf ("ret = %d\n ", num);
+    }
+    close (fd);
+    return 0; 
+}
